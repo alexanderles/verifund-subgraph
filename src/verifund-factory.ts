197f130c162802b4a68dbae82c45d50567dbc5ba
Alexander Les <awles642@gmail.com>
2022-09-25 11:39:58 -0400

    Subgraph for factory contract

--- src/verifund-factory.ts
+++ src/verifund-factory.ts
@@ -1,12 +1,15 @@
-import { CampaignCreated as CampaignCreatedEvent } from "../generated/VerifundFactory/VerifundFactory"
+import { 
+  CampaignCreated as CampaignCreatedEvent
+} from "../generated/VerifundFactory/VerifundFactory"
+
 import { CampaignCreated } from "../generated/schema"
+import { ByteArray, Bytes } from "@graphprotocol/graph-ts"
 
 export function handleCampaignCreated(event: CampaignCreatedEvent): void {
   let entity = new CampaignCreated(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
   entity.c = event.params.c
-  entity.whiteList = event.params.whiteList
   entity.targetAmount = event.params.targetAmount
   entity.save()
 }
